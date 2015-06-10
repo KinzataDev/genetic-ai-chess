9c5412e1d1fe7b01dbeba1ebd4c0f17b482e93f4
Maximilian Witte <maximilian.t.witte@gmail.com>
2015-06-09 18:12:35 -0600

    Add EnemyPiecesDefended gene

---
+++ lib/Chess/AI/Genetic/Gene/EnemyPiecesDefended.pm
@@ -0,0 +1,53 @@
+package Chess::AI::Genetic::Gene::EnemyPiecesDefended;
+
+use Moose;
+use namespace::autoclean;
+
+use Chess::Rep;
+
+extends 'Chess::AI::Genetic::Gene';
+
+has '+weight' => (
+	is => 'ro',
+	isa => 'Int',
+	lazy => 1,
+	default => sub {
+		return 1;
+	},
+);
+
+has '+debug' => (
+	is      => 'rw',
+	isa     => 'Bool',
+	lazy    => 1,
+	default => 1,
+);
+
+augment 'calculate_value' => sub {
+	my $self       = shift;
+	my $game_state = shift;
+	my $move = shift;
+
+	my $count = 0;
+
+	my $rep = $game_state->{rep};
+	my $player = $game_state->{op_status}{to_move};
+
+	foreach my $piece ( @{$game_state->{op_status}{pieces}} ) {
+		my $is_attacked = $rep->is_attacked($piece->{from}, $player);
+		$count++ if $is_attacked;
+	}
+
+	my $value = $self->weight * $count;
+
+	return $value;
+};
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+
+
+
